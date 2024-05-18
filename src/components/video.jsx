97c57b6e015f6781fe524f853beb711decc19202
Dheeraj Kumar <devdheeraj83@gmail.com>
2024-05-18 18:55:12 +0530

    Update video.jsx

--- src/components/video.jsx
+++ src/components/video.jsx
@@ -1,19 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import { gsap } from "gsap";
 import backImg1 from "../assets/videoBG.png";
 
 const Video = () => {
   const [isPlaying, setIsPlaying] = useState(false);
+  const videoContainerRef = useRef(null);
+  const iframeRef = useRef(null);
 
   const togglePlay = () => {
-    setIsPlaying(!isPlaying);
+    if (!isPlaying) {
+      gsap.to(videoContainerRef.current, {
+        scale: 1.1,
+        opacity: 0,
+        duration: 0.5,
+        onComplete: () => {
+          setIsPlaying(true);
+          gsap.fromTo(
+            iframeRef.current,
+            { scale: 0.9, opacity: 0 },
+            { scale: 1, opacity: 1, duration: 0.5 }
+          );
+        },
+      });
+    }
   };
 
   return (
-    <section className="text-white mt-10 body-font h-screen relative">
+    <section className="text-white mt-10 body-font h-screen relative flex justify-center items-center">
       {isPlaying ? (
-        <iframe className="w-full h-full max-w-full border rounded-lg border-gray-700" src="https://www.youtube.com/embed/3JABJHlY4Tw?si=hv_1FnVKyszhrCJk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe
+          ref={iframeRef}
+          className="w-3/4 h-2/3"        
+          src="https://www.youtube.com/embed/3JABJHlY4Tw?si=hv_1FnVKyszhrCJk"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
       ) : (
         <div
+          ref={videoContainerRef}
           className="w-full h-full bg-cover bg-center flex flex-col gap-16 justify-center items-center cursor-pointer relative"
           style={{ backgroundImage: `url(${backImg1})` }}
           onClick={togglePlay}
