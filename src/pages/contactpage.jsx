f07e401c225c92584a289b8188b5166e043bd7d5
Dheeraj Kumar <devdheeraj83@gmail.com>
2024-05-19 11:10:31 +0530

    Update contactpage.jsx

--- src/pages/contactpage.jsx
+++ src/pages/contactpage.jsx
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
-import { RiMailSendLine, RiCustomerServiceLine } from "react-icons/ri";
+import {
+  RiCustomerServiceLine,
+  RiMapPinLine,
+  RiGlobalLine,
+} from "react-icons/ri";
 
 const ContactPage = () => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
-    phone: "",
+    subject: "",
     message: "",
   });
 
@@ -17,135 +21,157 @@ const ContactPage = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
+    // Handle form submission
   };
 
   return (
     <>
       <Navbar />
       <div className="coverImg flex flex-col justify-center items-center h-40 md:h-[275px] w-full bg-cover">
-        <h1 className="md:text-6xl text-2xl m-4 uppercase font-semibold text-white">Connect <span className="text-gray-700">With Us</span></h1>
-        <h1 className="md:text-3xl  text-xl uppercase font-light text-gray-700">Just few inputs away</h1>
+        <h1 className="md:text-6xl text-2xl m-4 uppercase font-semibold text-white">
+          Connect <span className="text-gray-700">With Us</span>
+        </h1>
+        <h1 className="md:text-3xl  text-xl uppercase font-light text-gray-700">
+          Just few inputs away
+        </h1>
       </div>
-      <section className="flex flex-col-reverse md:flex-row justify-center items-center gap-4 md:gap-0 p-4 md:p-8 h-auto md:h-screen">
-        <div className="leftBar md:w-1/3 p-6 bg-gray-100 md:h-full h-auto shadow-lg">
-          <h1 className="text-xl md:text-3xl p-2 md:p-4 font-bold text-black">
-            Reach us <span className="text-darkGreen">through</span>
-          </h1>
-          <h4 className="pl-2 md:pl-4 text-sm md:text-base">
-            We're eager to receive your feedback. Our approachable team is available to chat anytime.
-          </h4>
-          <div className="pl-2 md:pl-4 flex gap-2 md:gap-4 items-center mt-4 md:mt-6">
-            <RiMailSendLine size={24} md:size={28} color="green" />
-            <h2 className="text-base md:text-2xl font-semibold">Via Email</h2>
-            <h2 className="text-base md:text-xl font-light">support@abhirig.co.in</h2>
+
+      <section className="bg-white">
+        <div className="max-w-screen-xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+          <div className="p-4 grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-6">
+            <div className="w-full flex-col flex items-center justify-center bg-white mx-auto md:p-12 shadow-md">
+              <RiMapPinLine color="green" className="w-24 h-24" />
+              <h3 className="text-2xl mt-4 uppercase font-semibold tracking-tighter">
+                Office Address
+              </h3>
+              <p className="mt-2 w-2/3 text-center text-gray-600">
+                99 NY Address Street, Brooklyn, NY, United States
+              </p>
+            </div>
+
+            <div className="w-full flex-col flex items-center justify-center bg-white mx-auto md:p-12 shadow-md">
+              <RiCustomerServiceLine color="green" className="w-24 h-24" />
+              <h3 className="text-2xl mt-4 uppercase font-semibold tracking-tighter">
+                Phone Number
+              </h3>
+              <p className="mt-2 w-2/3 text-center text-gray-600">
+                0122 2142 4241
+              </p>
+              <p className="text-center text-gray-600">765 648 5678</p>
+            </div>
+
+            <div className="w-full flex-col flex items-center justify-center bg-white mx-auto md:p-12 shadow-md">
+              <RiGlobalLine color="green" className="w-24 h-24" />
+              <h3 className="text-2xl mt-4 uppercase font-semibold tracking-tighter">
+                Web Connect
+              </h3>
+              <p className="mt-2 w-2/3 text-center text-gray-600">
+                info@suxnixmail.com
+              </p>
+              <p className="text-center text-gray-600">suxnixexample.com</p>
+            </div>
           </div>
-          <div className="pl-2 md:pl-4 flex gap-2 md:gap-4 items-center mt-2 md:mt-6">
-            <RiCustomerServiceLine size={24} md:size={28} color="green" />
-            <h2 className="text-base md:text-2xl font-semibold">Via Phone</h2>
-            <h2 className="text-base md:text-xl font-light">+919998881234</h2>
+
+          <div className="mt-12 flex flex-col lg:flex-row">
+            <div className="relative lg:w-2/3 shadow-md overflow-hidden">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3023.7431122034714!2d-73.98011378459335!3d40.6846297793347!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25bae14e31f7b%3A0x65e1a2d8ff0d2479!2s620%20Atlantic%20Ave%2C%20Brooklyn%2C%20NY%2011217%2C%20USA!5e0!3m2!1sen!2s!4v1629988294780!5m2!1sen!2s"
+                width="100%"
+                height="100%"
+                className="absolute inset-0 border-0"
+                allowFullScreen=""
+                loading="lazy"
+              ></iframe>
+            </div>
+            <div className="mt-8 lg:mt-0 lg:w-3/4 bg-white p-6">
+              <h3 className="md:text-5xl text-lg p-4  uppercase font-semibold tracking-tighter mb-4">
+                Asked Anything You Want to Know
+              </h3>
+              <form onSubmit={handleSubmit} className="">
+                <div className="mb-4 p-2 flex gap-4">
+                  {" "}
+                  <div className="w-1/2">
+                    {" "}
+                    <label
+                      htmlFor="name"
+                      className="block text-md font-medium text-gray-700"
+                    >
+                      Full Name
+                    </label>
+                    <input
+                      type="text"
+                      id="name"
+                      placeholder="Enter Here"
+                      name="name"
+                      value={formData.name}
+                      onChange={handleChange}
+                      className="mt-2 py-3 p-2 w-full border focus:outline-none focus:ring-2 focus:ring-green-500 border-gray-300"
+                      required
+                    />
+                  </div>
+                  <div className="w-1/2">
+                    {" "}
+                    {/* Assign half width for each input */}
+                    <label
+                      htmlFor="email"
+                      className="block text-md font-medium text-gray-700"
+                    >
+                      Email Address
+                    </label>
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      placeholder="Enter Here"
+                      value={formData.email}
+                      onChange={handleChange}
+                      className="mt-2 py-3 p-2 w-full border focus:outline-none focus:ring-2 focus:ring-green-500 border-gray-300"
+                      required
+                    />
+                  </div>
+                </div>
+
+                <div className="mb-4 px-2">
+                  <select
+                    id="subject"
+                    name="subject"
+                    value={formData.subject}
+                    onChange={handleChange}
+                    className="mt-1 p-2 py-3 w-full border focus:outline-none focus:ring-2 focus:ring-green-500 border-gray-300"
+                    required
+                  >
+                    <option value="">Select Subject</option>
+                    <option value="feedback">Feedback</option>
+                    <option value="support">Support</option>
+                    <option value="general">General Inquiry</option>
+                  </select>
+                </div>
+                <div className="mb-4  p-2">
+                  <label
+                    htmlFor="message"
+                    className="block text-md font-medium text-gray-700"
+                  >
+                    Message
+                  </label>
+                  <textarea
+                    id="message"
+                    name="message"
+                    value={formData.message}
+                    onChange={handleChange}
+                    rows="6"
+                    className="mt-2  p-2 w-full border  focus:outline-none focus:ring-2 focus:ring-green-500 border-gray-300"
+                    required
+                  ></textarea>
+                </div>
+                <button
+                  type="submit"
+                  className="bg-green-500 rounded-full text-white px-8 p-4 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500"
+                >
+                  Make Request
+                </button>
+              </form>
+            </div>
           </div>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d462560.6828087052!2d54.89783255663514!3d25.07628045139577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f43496ad9c645%3A0xbde66e5084295162!2sDubai%20-%20United%20Arab%20Emirates!5e0!3m2!1sen!2sin!4v1714739075247!5m2!1sen!2sin"
-            width="100%"
-            height="70%"
-            style={{ border: "0" }}
-            allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-            className="mt-4"
-          ></iframe>
-        </div>
-        <div className="rightBar md:w-1/3 md:h-full p-6 shadow-lg">
-          <h1 className="text-xl md:text-4xl p-2 md:p-4 font-bold text-darkGreen">
-            Get In Touch
-          </h1>
-          <h1 className="text-base md:text-3xl pl-2 md:pl-4 font-bold text-black">
-            Connect with a single click
-          </h1>
-          {/* Form */}
-          <form className="mt-4 md:mt-8" onSubmit={handleSubmit}>
-            <div className="mb-2 md:mb-4">
-              <label
-                htmlFor="name"
-                className="block text-sm md:text-base font-semibold text-gray-700"
-              >
-                Name
-              </label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                className={`mt-1 p-2 w-full border rounded-md focus:outline-none ${
-                  formData.name ? "border-green-500" : "border-gray-400"
-                }`}
-                required
-              />
-            </div>
-            <div className="mb-2 md:mb-4">
-              <label
-                htmlFor="email"
-                className="block text-sm md:text-base font-semibold text-gray-700"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                className={`mt-1 p-2 w-full border rounded-md focus:outline-none ${
-                  formData.email ? "border-green-500" : "border-gray-400"
-                }`}
-                required
-              />
-            </div>
-            <div className="mb-2 md:mb-4">
-              <label
-                htmlFor="phone"
-                className="block text-sm md:text-base font-semibold text-gray-700"
-              >
-                Phone
-              </label>
-              <input
-                type="tel"
-                id="phone"
-                name="phone"
-                value={formData.phone}
-                onChange={handleChange}
-                className={`mt-1 p-2 w-full border rounded-md focus:outline-none ${
-                  formData.phone ? "border-green-500" : "border-gray-400"
-                }`}
-                required
-              />
-            </div>
-            <div className="mb-2 md:mb-4">
-              <label
-                htmlFor="message"
-                className="block text-sm md:text-base font-semibold text-gray-700"
-              >
-                Message
-              </label>
-              <textarea
-                id="message"
-                name="message"
-                value={formData.message}
-                onChange={handleChange}
-                rows="8"
-                className="mt-1 p-2 w-full border rounded-md focus:outline-none border-gray-400"
-              ></textarea>
-            </div>
-            <button
-              type="submit"
-              className="w-full bg-darkGreen text-white py-2 px-4 rounded-md hover:bg-green-700 focus:outline-none"
-            >
-              Submit
-            </button>
-          </form>
-          {/* End of Form */}
         </div>
       </section>
       <Footer />
