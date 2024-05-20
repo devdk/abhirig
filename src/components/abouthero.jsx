b79f0fcade2009bff6c04887e3766934b09b049f
Dheeraj Kumar <devdheeraj83@gmail.com>
2024-05-20 11:37:46 +0530

    Update abouthero.jsx

--- src/components/abouthero.jsx
+++ src/components/abouthero.jsx
@@ -54,7 +54,7 @@ export default function Abouthero() {
           </div>
         </div>
       </section>
-      <section className="text-gray-600  body-font">
+{/*       <section className="text-gray-600  body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font text-2xl md:text-4xl mb-4 font-medium uppercase text-gray-900">Our Vision.. Our Passion 
@@ -66,7 +66,7 @@ export default function Abouthero() {
             <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"/>
           </div>
         </div>
-      </section>
+      </section> */}
     </>
   );
 }
