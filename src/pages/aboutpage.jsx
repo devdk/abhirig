23a5fa871703379019d0122600b3ca5642d3739e
Dheeraj Kumar <devdheeraj83@gmail.com>
2024-05-20 11:11:51 +0530

    Update aboutpage.jsx

--- src/pages/aboutpage.jsx
+++ src/pages/aboutpage.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 import Navbar from "../components/navbar";
 import Abtftr from "../components/aboutfeature";
 import Abouthero from "../components/abouthero";
